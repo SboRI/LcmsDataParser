a4e1c366e1cc0b0b6fd2be4bbf8d173f7735bb6c
Sebastian Bormannm <sb.bormann@gmail.com>
2018-04-02 18:38:32 +0200

    added commentry

--- replaceNames.py
+++ replaceNames.py
@@ -39,27 +39,38 @@ perAcid = {acid: meanDev[acid].sort_values('mean', ascending=False) for acid in 
 
 # For each acid in importantAcids
 for acid in perAcid:
-    # sort concentration in descening order. Take best 10.
+    # sort concentration in descening order. Take best 10. remove negative concentrations
     perAcid[acid] = perAcid[acid].reset_index().head(10)
     perAcid[acid] = perAcid[acid].loc[perAcid[acid]['mean'] > 0]
-    #print()
 
     #Dataframe for saving sum of acids
     sumOfAcids = pd.DataFrame()
+
     #get the sum of all acids for this sample and timepoint
     for index, row in perAcid[acid].iterrows():
         allAcid = meanDev.loc[(row['Name'], row["Zeitpunkt"])].xs('mean', level=1).sum()
         allAcidDf = pd.DataFrame({'Name': [row['Name']], "Zeitpunkt": [row["Zeitpunkt"]], 'sumOfAcids': [allAcid]})
         sumOfAcids=sumOfAcids.append(allAcidDf)
 
+    # add sum of all acids to result table
     perAcid[acid] = perAcid[acid].merge(sumOfAcids, on=['Name', 'Zeitpunkt'])
+    # add ratio to results
     perAcid[acid]['ratio'] = perAcid[acid]['mean']/perAcid[acid]['sumOfAcids']
     
+    #plotting
+
+    #select relevant columns
+    #TODO: error bars
     ax=perAcid[acid][['Name', 'mean', 'ratio']].set_index('Name').plot(kind='bar', secondary_y=["ratio"], title=acid)
+    #Axis labels
     ax.set_ylabel("Concentration")
     ax.right_ax.set_ylabel("Ratio")
+    #Axis limits
     ax.right_ax.set_ylim((0,1))
 
+    #save figure without unallowed system characters
     plt.savefig(acid.replace("/", "")+"_figure"+".pdf")
+    
+    
     print(acid)
     print(perAcid[acid])
